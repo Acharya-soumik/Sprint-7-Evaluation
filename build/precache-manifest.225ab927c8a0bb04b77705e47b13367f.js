915a94cc6b974e26828530d0fa88749da1f889ef
Soumik Acharjee <acharjee266@gmail.com>
2020-02-10 19:41:20 +0530

    adding build file

---
+++ build/precache-manifest.225ab927c8a0bb04b77705e47b13367f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f95e4b29023bb7e006d2d8483222da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ccc5017f2114c99561",
+    "url": "/static/css/main.b201ec8b.chunk.css"
+  },
+  {
+    "revision": "e6541e59c87002a9fa9c",
+    "url": "/static/js/2.04d9eb2e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.04d9eb2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ccc5017f2114c99561",
+    "url": "/static/js/main.f902cad8.chunk.js"
+  },
+  {
+    "revision": "eaab6d9ce58e700a5381",
+    "url": "/static/js/runtime-main.e048dab8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
